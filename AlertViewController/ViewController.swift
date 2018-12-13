49f4123f2db5f349f01d8e4a7a825dd3feedbc88
jayaram G <43853836+Jayaram143@users.noreply.github.com>
2018-12-13 12:50:06 +0530

    Added AlertView

--- AlertViewController/ViewController.swift
+++ AlertViewController/ViewController.swift
@@ -10,11 +10,38 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBAction func actionBtn(_ sender: Any) {
+        
+    AlertAction()
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
 
+    func AlertAction(){
+        
+        let alert = UIAlertController(title: "Jayaram", message: "IOS developer", preferredStyle: .alert)
+      
+        alert.addAction(UIAlertAction.init(title: "Profile", style: .default, handler: nil))
+        
+        
+     self.present(alert, animated: true ,completion: nil)
+        
+        
+        
+        
+        
+        
+    }
+    
+    
+    
+    
+    
+    
 }
 
