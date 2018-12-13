da046cbff6becf8535f3e08256591c120492f159
jayaram G <43853836+Jayaram143@users.noreply.github.com>
2018-12-13 13:03:36 +0530

    Added sample TableView

---
+++ AlertViewController/customCell.swift
@@ -0,0 +1,24 @@
+//
+//  customCell.swift
+//  AlertViewController
+//
+//  Created by Jayaram G on 13/12/18.
+//  Copyright © 2018 Jayaram G. All rights reserved.
+//
+
+import UIKit
+
+class customCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
